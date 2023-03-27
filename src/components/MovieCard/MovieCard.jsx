fe26bf01f6cb30850f3d46d950e76c8495ae8eb2
Gregory <maklakov.grigory@gmail.com>
2023-03-27 15:39:08 +0200

    fix build bugs

---
+++ src/components/MovieCard/MovieCard.jsx
@@ -0,0 +1,18 @@
+import PropTypes from 'prop-types'
+import styles from './MovieCard.module.scss';
+import { Link } from 'react-router-dom';
+import { Button } from '../Button';
+import { tmdbApi, apiConfig } from "../../api/apiClient";
+
+
+export const MovieCard = ({ item }) => {
+
+
+
+    return (
+        <div>MovieCard</div>
+    )
+}
+
+MovieCard.propTypes = {}
+
